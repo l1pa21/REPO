5cd95a460d8f000b78250878ac6aee6bf55c0c9d
zzz57 <zzz57@LAPTOP-ABGFVAI3>
2025-09-30 11:03:03 +0500

    TASK01

---
+++ TASK01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            string a = "Александр Блок";
+            Console.WriteLine(a);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            string b = "Чудесно всё, что узнаю?..";
+            Console.WriteLine(b);
+            Console.ForegroundColor = ConsoleColor.Red;
+            string c = "Чудесно всё, что узнаю? ,\nПостыдно всё, что совершаю.\nГотов идти навстречу раю,k01\nИ медлю в сумрачном краю.";
+            Console.WriteLine(c);
+            Console.ResetColor();
+        }
+    }
+}
