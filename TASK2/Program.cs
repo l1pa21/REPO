e1940080cc70c171cba94ba83ad214a5b6b3d8ce
zzz57 <zzz57@LAPTOP-ABGFVAI3>
2025-09-30 21:51:24 +0500

    TASK02

---
+++ TASK2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод данных
+        Console.Write("Введите длину стороны a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите длину стороны b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите угол между ними в градусах: ");
+        double angleDegrees = double.Parse(Console.ReadLine());
+
+        // Переводим угол в радианы
+        double angleRadians = angleDegrees * Math.PI / 180.0;
+
+        // Вычисляем площадь
+        double area = 0.5 * a * b * Math.Sin(angleRadians);
+
+        // Находим третью сторону по теореме косинусов
+        double c = Math.Sqrt(a * a + b * b - 2 * a * b * Math.Cos(angleRadians));
+
+        // Вычисляем периметр
+        double perimeter = a + b + c;
+
+        // Выводим результат
+        Console.WriteLine($"Площадь треугольника: {area:F2}");
+        Console.WriteLine($"Периметр треугольника: {perimeter:F2}");
+    }
+}
