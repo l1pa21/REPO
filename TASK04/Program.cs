ee15de453614bcb3c86d32d675b49274bbd85093
zzz57 <zzz57@LAPTOP-ABGFVAI3>
2025-10-01 22:34:41 +0500

    TASK04

---
+++ TASK04/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        double y = 1.0 / (x * x + 1.0 / (x * x + 1.0 / (x * x + 1.0)));
+
+        Console.WriteLine($"y = {y}");
+    }
+}
