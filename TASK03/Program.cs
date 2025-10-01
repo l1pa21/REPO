3548d884a6ffd23691744ccb1f5743d144bf2cf2
zzz57 <zzz57@LAPTOP-ABGFVAI3>
2025-10-01 20:19:25 +0500

    TASK03

---
+++ TASK03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите k (1 ≤ k ≤ 150): ");
+        int k = int.Parse(Console.ReadLine());
+
+        if (k < 1 || k > 150)
+        {
+            Console.WriteLine("Ошибка: k должно быть в пределах 1 ≤ k ≤ 150");
+        }
+        else
+        {
+            int number = 100 + k;
+            Console.WriteLine($"Число, которое стоит на {k}-м месте последовательности: {number}");
+        }
+    }
+}
