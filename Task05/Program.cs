4983465b8971f058df5335cf03dbfcb404bcaf1e
zzz57 <zzz57@LAPTOP-ABGFVAI3>
2025-10-08 20:26:50 +0500

    TASK05

---
+++ Task05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task05
+{
+    internal class Program
+    {
+        static double CalcPart(double a, double b, double c)
+        {
+            return (Math.Sin(a) - Math.Cos(b)) / Math.Sqrt(c);
+        }
+
+        static void Main()
+        {
+            double x = CalcPart(2, 3, 5) + CalcPart(3, 4, 7) + CalcPart(5, 6, 11);
+            Console.WriteLine($"x = {x:F3}");
+        }
+    }
+}
