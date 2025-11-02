656eebedd5e527b551dd5b8239546df7b47bf78d
zzz57 <zzz57@LAPTOP-ABGFVAI3>
2025-11-02 15:20:59 +0500

    task09

---
+++ task09/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static double CalculateF(double x)
+    {
+        if (Math.Abs(x) > 1)
+        {
+            return 1;
+        }
+        else if (x > 0 && Math.Abs(x) <= 1)
+        {
+            return Math.Abs(x);
+        }
+        else // x = 0
+        {
+            return 2;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите x: ");
+        double x = double.Parse(Console.ReadLine());
+        double result = CalculateF(x);
+        Console.WriteLine($"f({x}) = {result}");
+    }
+}
