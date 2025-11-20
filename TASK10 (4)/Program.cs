e04b3ff2da40e56d013696605f583ba3b526c283
zzz57 <zzz57@LAPTOP-ABGFVAI3>
2025-11-20 22:53:57 +0500

    TASK10 (4)

---
+++ TASK10 (4)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Задание 1: Введите натуральное число:");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите цифру k для подсчета в десятичной записи числа:");
+        char k = Console.ReadLine()[0];
+
+        string decimalNumber = number.ToString();
+
+        int count = 0;
+        foreach (char c in decimalNumber)
+        {
+            if (c == k) count++;
+        }
+
+        Console.WriteLine($"Цифра {k} встречается {count} раз(а) в десятичной записи числа {number}.");
+    }
+}
