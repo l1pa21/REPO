ee1f48db95644be90558920e961e4d107f195381
zzz57 <zzz57@LAPTOP-ABGFVAI3>
2025-11-20 22:20:45 +0500

    TASK10

---
+++ task10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int m, n;
+
+        Console.Write("Введите m: ");
+        while (!int.TryParse(Console.ReadLine(), out m))
+        {
+            Console.Write("Ошибка! Введите ЧИСЛО для m: ");
+        }
+
+        Console.Write("Введите n: ");
+        while (!int.TryParse(Console.ReadLine(), out n))
+        {
+            Console.Write("Ошибка! Введите ЧИСЛО для n: ");
+        }
+
+        long result = 1;
+
+        for (int i = m; i <= n; i++)
+        {
+            result *= i;
+        }
+
+        Console.WriteLine($"Произведение = {result}");
+        Console.ReadLine();
+    }
+}
