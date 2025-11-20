ffdc391dd9d95e3e20cbe6106d3ee4bfddc8c37c
zzz57 <zzz57@LAPTOP-ABGFVAI3>
2025-11-20 23:06:47 +0500

    TASK10 (6)

---
+++ TASK10 (6)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите количество первых простых чисел n:");
+        int n = int.Parse(Console.ReadLine());
+
+        int found = 0;
+        int current = 2;
+
+        Console.WriteLine($"Первые {n} простых чисел:");
+        while (found < n)
+        {
+            if (IsPrime(current))
+            {
+                Console.Write(current + " ");
+                found++;
+            }
+            current++;
+        }
+    }
+
+    static bool IsPrime(int num)
+    {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
