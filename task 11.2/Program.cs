9131cdcc4e37f99666f02f843effe8e0f2da61b2
zzz57 <zzz57@LAPTOP-ABGFVAI3>
2026-01-06 22:34:04 +1000

    TASK 11.2

---
+++ task 11.2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите n: ");
+        int n = int.Parse(Console.ReadLine() ?? "0");
+
+        double[] a = CreateRandomArray(n);
+
+        Console.WriteLine("Исходный массив:");
+        PrintArray(a);
+
+        DivideBySum(a);
+
+        Console.WriteLine("После деления на сумму:");
+        PrintArray(a);
+    }
+
+    static double[] CreateRandomArray(int n)
+    {
+        double[] arr = new double[n];
+        Random rnd = new Random();
+        for (int i = 0; i < n; i++) arr[i] = rnd.NextDouble();
+        return arr;
+    }
+
+    static void DivideBySum(double[] arr)
+    {
+        double sum = 0;
+        for (int i = 0; i < arr.Length; i++) sum += arr[i];
+
+        if (sum == 0) return;
+
+        for (int i = 0; i < arr.Length; i++) arr[i] /= sum;
+    }
+
+    static void PrintArray(double[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write($"{arr[i]:F4}");
+            if (i < arr.Length - 1) Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
