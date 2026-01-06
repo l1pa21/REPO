db6855ac66b0fa059762afdcd0d833512b784638
zzz57 <zzz57@LAPTOP-ABGFVAI3>
2026-01-06 22:30:22 +1000

    TASK 11.1

---
+++ task11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите n: ");
+        int n = int.Parse(Console.ReadLine() ?? "0");
+
+        double[] a = new double[n];
+        Random rnd = new Random();
+
+        for (int i = 0; i < n; i++)
+            a[i] = rnd.NextDouble();   // [0;1)
+
+        PrintArray(a);
+    }
+
+    static void PrintArray(double[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write($"{arr[i]:F4}");
+            if (i < arr.Length - 1) Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
