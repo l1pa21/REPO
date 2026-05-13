92d2ba8f31716a94e5c11f53810ad019f467bb0c
zzz57 <zzz57@NOTEBOOK21>
2026-05-13 23:53:12 +0500

    Выполнено задание 14: добавлена иерархия медработников и полиморфизм

---
+++ HRLibrary/HRLibrary/MedicalWorker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HRLibrary
+{
+    public abstract class MedicalWorker
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Department { get; set; } // Отделение
+
+        public MedicalWorker(string name, string surname, string department)
+        {
+            Name = name;
+            Surname = surname;
+            Department = department;
+        }
+        public abstract string GetInfo();
+    }
+}
--- HRLibrary/HRLibrary/Patient.cs
+++ HRLibrary/HRLibrary/Patient.cs
@@ -7,29 +7,39 @@ namespace HRLibrary
         public string Name { get; set; }
         public string Surname { get; set; }
         public readonly string PolicyNumber;
-
         public DateTime AdmissionDate { get; set; }
         public DateTime DischargeDate { get; set; }
         public decimal TreatmentCost { get; set; }
         public PatientServiceType Service { get; set; }
 
+        public MedicalWorker AttendingStaff { get; set; }
+
         public Patient(string name, string surname, string policyNumber, PatientServiceType service)
         {
             Name = name;
             Surname = surname;
-            PolicyNumber = policyNumber; 
+            PolicyNumber = policyNumber;
             Service = service;
             AdmissionDate = DateTime.Now;
         }
 
         public virtual string[] GetInfo()
         {
-            var info = new string[2];
+            var info = new string[3];
+
             info[0] = $"{Name} {Surname}";
 
             string serviceName = Service == PatientServiceType.Insurance ? "страховое" : "платное";
-
             info[1] = $"Полис: {PolicyNumber}. Тип: {serviceName}. Поступил: {AdmissionDate:d}.";
+            if (AttendingStaff != null)
+            {
+                info[2] = $"Лечащий персонал: {AttendingStaff.GetInfo()}";
+            }
+            else
+            {
+                info[2] = "Персонал не назначен";
+            }
+
             return info;
         }
     }
