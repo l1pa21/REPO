7118d53f59c8bb219c830098ee6dcfc8240ce2a7
zzz57 <zzz57@NOTEBOOK21>
2026-06-13 15:17:18 +0500

    решение для задания 13 (нужный вариант №12)

---
+++ TourLibrary/TourLibrary/Tour.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace TourLibrary
+{
+    public class Tour
+    {
+        public string Name { get; set; }
+
+        public readonly string Code;
+
+        public string Places { get; set; }
+
+        public TransportType Transport { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate => StartDate + Duration;
+
+        public string Description { get; set; }
+
+        public Tour(
+            string name,
+            string code,
+            string places,
+            TransportType transport,
+            TimeSpan duration)
+        {
+            Name = name;
+            Code = code;
+            Places = places;
+            Transport = transport;
+            Duration = duration;
+        }
+
+        public virtual string[] GetInfo()
+        {
+            string[] info = new string[2];
+
+            info[0] = $"{Name} ({Code})";
+
+            info[1] =
+                $"Места посещения: {Places}. " +
+                $"Транспорт: {Transport}. " +
+                $"Продолжительность: {Duration.Days} дней.";
+
+            return info;
+        }
+    }
+}
